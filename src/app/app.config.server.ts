cf656eaa0bdc816e74617b9f584a4ba5cd48f164
ENPei <labibi.lg@gmail.com>
2026-07-01 15:36:06 +0800

    fix: 調整SSR取的環境變數方式

--- src/app/app.config.server.ts
+++ src/app/app.config.server.ts
@@ -2,10 +2,13 @@ import { mergeApplicationConfig, ApplicationConfig } from '@angular/core';
 import { provideServerRendering, withRoutes } from '@angular/ssr';
 import { appConfig } from './app.config';
 import { serverRoutes } from './app.routes.server';
+import { provideApiConfiguration } from './api/api-configuration';
+import { environment } from '../environments/environment';
 
 const serverConfig: ApplicationConfig = {
   providers: [
-    provideServerRendering(withRoutes(serverRoutes))
+    provideServerRendering(withRoutes(serverRoutes)),
+    provideApiConfiguration(environment.apiBaseUrl),
   ]
 };
 
